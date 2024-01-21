da90b083a12420ea7dd527fa74576cc20561a336
Dana Popa <popa.dana100@gmail.com>
2024-01-21 22:56:16 +0200

    first commit

---
+++ week_2/numbers_01.js
@@ -0,0 +1,21 @@
+// Complete the function convertNumberToString such that it converts the number it receives into a string.
+
+//function convertNumberToString(number) {}
+
+
+//let convertNumberToString = 42;
+//let convertNumberToString = 97;
+let convertNumberToString = 11;
+let stringAnswer = convertNumberToString.toString();
+console.log(stringAnswer);
+
+// Complete the function convertStringToNumber such that it converts the string it receives into a number.
+
+//function convertStringToNumber(string) {}
+
+//let convertStringToNumber = "42";
+//let convertStringToNumber = "97";
+let convertStringToNumber = "11";
+let numericValue = Number.parseInt(convertStringToNumber, 10);
+console.log(numericValue);
+
