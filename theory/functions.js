e5a9217fc581ed64442cc3df30907eca2207834a
Dana Popa <popa.dana100@gmail.com>
2024-01-21 19:15:23 +0200

    basic functions

---
+++ theory/functions.js
@@ -0,0 +1,9 @@
+/* function sum(x, y) {
+    return x + y;
+  }
+  console.log (sum(1, 3));
+*/
+
+const isLegal = (age) => age >= 18;
+console.log (isLegal(17));
+console.log (isLegal(20));
--- week_2/variables_01.js
+++
@@ -1,5 +0,0 @@
-//Define a variable called count with an original value of 0 and then increment it (add 1 to it) on th efollowing line.
-
-let count = 0;
-
-console.log(count);
