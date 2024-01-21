f7e3211cf576fe986b50cc48f34ab99c4d0f2a04
Dana Popa <popa.dana100@gmail.com>
2024-01-21 20:09:52 +0200

    numbers

--- theory/numbers.js
+++ theory/numbers.js
@@ -3,6 +3,13 @@ let stringAnswer = answer.toString();
 console.log(stringAnswer);
 */
 
-let str = "42";
+/*let str = "42";
 let numericValue = Number.parseInt(str, 10);
 console.log(numericValue);
+*/
+
+let x = 15;
+let y = "10";
+y = Number.parseInt(y, 10);
+let sub = x - y;
+console.log(sub);
