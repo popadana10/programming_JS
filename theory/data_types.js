c2c56d9e5a911369698167cf576456ca778a6943
Dana Popa <popa.dana100@gmail.com>
2024-01-21 20:02:25 +0200

    numbers, data types

---
+++ theory/data_types.js
@@ -0,0 +1,16 @@
+/*let name = 'Alice';
+let age = 30;
+let isStudent = false;
+let hobbies = ['reading', 'cycling'];
+
+console.log(typeof name); // "string"
+console.log(typeof age); // "number"
+console.log(typeof isStudent); // "boolean"
+console.log(typeof hobbies); // "object" (arrays are type 'object' in JavaScript)
+*/
+
+
+/*let notDefined; // Undefined
+let emptyValue = null; // Null
+console.log(typeof notDefined);
+*/
---
+++ theory/numbers.js
@@ -0,0 +1,8 @@
+/*let answer = 42;
+let stringAnswer = answer.toString();
+console.log(stringAnswer);
+*/
+
+let str = "42";
+let numericValue = Number.parseInt(str, 10);
+console.log(numericValue);
