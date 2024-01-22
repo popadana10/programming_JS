8c89a9155af851034b08e6427e0d419b67ee89dd
Dana Popa <popa.dana100@gmail.com>
2024-01-22 14:30:35 +0200

    strings

---
+++ week_2/strings_03.js
@@ -0,0 +1,11 @@
+/**
+Refine the lowerName function to return the name parameter it receives, entirely in lowercase.
+
+ */
+
+function lowerName(name) {
+    return "ALEX".toLowerCase();
+}
+
+// console.log(lowerName('Sam')); 
+console.log(lowerName('ALEX')); 
