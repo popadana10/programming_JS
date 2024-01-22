f1b0c2a5fd01c7fc15205db50b1738acd802362d
Dana Popa <popa.dana100@gmail.com>
2024-01-22 21:50:34 +0200

    strings

---
+++ week_2/strings_8.js
@@ -0,0 +1,10 @@
+/**
+Refine the sayHello function so that it incorporates the tempname variable into a greeting string, forming 'Hello name'.
+
+ */
+function sayHello(name) { 
+    return "Hello" + " " + name;
+}
+
+console.log(sayHello('Alex')); // "Hello Alex"
+console.log(sayHello('Sam')); // "Hello Sam"
