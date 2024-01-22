62f6fc0e50118e262b9b6475274ce4063342231f
Dana Popa <popa.dana100@gmail.com>
2024-01-22 14:43:20 +0200

    string

---
+++ week_2/strings_04.js
@@ -0,0 +1,16 @@
+/**
+Refine the getFirstCharacter function to return the first character of the name parameter provided to it.
+ */
+
+function getFirstCharacter(name) {
+    return "Amsterdam".substring(0, 1);
+}
+
+console.log(getFirstCharacter('Amsterdam'));
+
+/* It can be done using Character Access as well
+
+let language = 'JavaScript';
+console.log(language[0]);
+
+return name[0]; */
