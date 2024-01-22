870a7ba0c317bea5cf904c7a217643fe7b4cbc43
Dana Popa <popa.dana100@gmail.com>
2024-01-22 14:47:37 +0200

    strings

---
+++ week_2/string_05.js
@@ -0,0 +1,19 @@
+/**
+Refine the getLastCharacter function so that it returns the last character from the name parameter it receives.
+ */
+
+function getLastCharacter(name) {
+    return "Sam".substring(2, 3);
+} 
+
+
+console.log(getLastCharacter('Sam')); // "m"
+// console.log(getLastCharacter('Alex')); // "x"
+// console.log(getLastCharacter('Charley')); // "y"
+
+/* It can be done using Character Access as well
+
+let language = 'JavaScript';
+console.log(language[0]);
+
+return name[2]; */
