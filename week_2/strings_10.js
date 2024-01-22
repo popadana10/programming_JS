d273e9f824607a65f101839d73acfd393b6d495e
Dana Popa <popa.dana100@gmail.com>
2024-01-22 22:20:43 +0200

    strings

--- week_2/strings_10.js
+++ week_2/strings_10.js
@@ -5,6 +5,7 @@ Refine the capitalize function to ensure it capitalizes the first letter of the 
 function capitalize(word) {
     return word[0].toUpperCase() + word.slice(1).toLowerCase();
 }
+
 console.log(capitalize('sam')); // "Sam"
 console.log(capitalize('ALEX')); // "Alex"
 console.log(capitalize('chARLie')); // "Charlie"
