dec6d64748b1e2596320fa155e91f80c209c1d9d
Dana Popa <popa.dana100@gmail.com>
2024-01-23 20:56:46 +0200

    conditions

---
+++ conditions_06.js
@@ -0,0 +1,20 @@
+/**
+Develop a function called greaterNum which:
+
+ - Accepts two arguments, both of which are numbers.
+ - Returns the larger of the two numbers.
+
+ */
+
+function greaterNum(x, y) {
+    if ( x > y) {
+        return x;
+    } else {
+        return y;
+       }
+}
+
+// Sample usage - do not modify
+console.log(greaterNum(5, 10));
+console.log(greaterNum(2, 1));
+console.log(greaterNum(4, 25));
