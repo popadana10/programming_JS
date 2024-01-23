fa3c9946d129f306aa21b1f7bd42df96905957d1
Dana Popa <popa.dana100@gmail.com>
2024-01-23 20:53:06 +0200

    conditions

---
+++ conditions_02.js
@@ -0,0 +1,17 @@
+/*
+Create a program getTemperature that asks the user to input a temperature in Celsius and then outputs whether they should wear a jacket (if the temperature is below 15°C) or not.
+ */
+
+function getTemperature(temperature) {
+  
+   temperature = Number(prompt('What is the current temperature in Celsius?'));
+
+    if (temperature < 15) {
+        return ('Wear a jacket! It is cold outside.');
+      } else {
+       return ('No need to wear a jacket.');
+      }
+} 
+
+getTemperature(Number(prompt('What is the current temperature?'))); 
+
