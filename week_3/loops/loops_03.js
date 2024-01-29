c878fcdec0ee33f73e18663e8b5b16e19bef2ab3
Dana Popa <popa.dana100@gmail.com>
2024-01-29 14:00:22 +0200

    commit

--- week_3/loops/loops_03.js
+++ week_3/loops/loops_03.js
@@ -1,1 +1,15 @@
 // Create a program that continuously prompts the user to input distance (in kilometers) and time (in hours) and then calculates the average speed. The program should terminate when the user enters 0 for the distance. Upon receiving a distance of 0, the program should not prompt for any further input.
+
+let distance, time;
+
+while (distance!= 0) {
+    distance = Number(prompt('Enter distance in kilometers'));
+
+    if (distance == 0) {
+        console.log('distance 0 - game over')
+        break;
+    }
+    time = Number(prompt('Enter time in hours'));
+    let average = distance/time;
+    console.log('average is:', average);
+}
