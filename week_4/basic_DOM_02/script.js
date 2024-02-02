774f9e82ed51942e5e57d70f2125444b55499202
Dana Popa <popa.dana100@gmail.com>
2024-02-02 17:32:56 +0200

    exercise 2

---
+++ week_4/basic_DOM_02/script.js
@@ -0,0 +1,30 @@
+/* 
+
+Write JavaScript code that adds an event listener to the button.
+
+When the button is clicked, the function should:
+- Retrieve the value from the input field.
+- Create a new <li> element and set its text content to the input field's value.
+- Append the new <li> element to the fruitList <ul>.
+
+Ensure the input field is cleared after adding the fruit to the list.
+
+*/
+
+    document.querySelector('#addFruitBtn').addEventListener('click', function addFruitToList() {
+
+        const inputValue = document.querySelector('#fruitInput'); //selecting the ID 
+        const fruitList = document.querySelector('#fruitList'); // selecting the ID
+
+        const newFruit = document.createElement('li'); 
+        const newFruitName = document.createTextNode(inputValue.value); 
+
+        newFruit.appendChild(newFruitName); //appendChild() is a method that adds a node at the end of the list of children of a given parent
+        fruitList.appendChild(newFruit);
+        
+        inputValue.value = ''; // to clear the input field after clicking the btn
+    });
+
+    console.log(addFruitToList());
+
+    // VICTOOOOOORYYYYYYYY!!!!! YEEES!!
