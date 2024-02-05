5feedf11d04079a29d91b53150085f2a0317769e
Dana Popa <popa.dana100@gmail.com>
2024-02-05 19:14:57 +0200

    update

--- week_4/basic_DOM_04/script.js
+++ week_4/basic_DOM_04/script.js
@@ -1,4 +1,4 @@
-
+/*
 
     document.addEventListener('input', function() { 
 
@@ -35,3 +35,30 @@
     
     console.log(calculateTotalPrice());
 
+*/
+
+
+const form = document.querySelector('.form-container');
+
+const priceCalc = () => {
+    // console.log('frwbfv');
+
+    const typeSelect = document.querySelector('#type');
+    const checkboxes = document.querySelectorAll("input[type='checkbox']");
+    const priceBanner = document.getElementById("totalPrice");
+
+    // console.log(typeSelect.value);
+    // console.log(checkboxes);
+
+    let totalAmount = parseInt(typeSelect.value);
+    // console.log(totalAmount);
+
+    for (const checkbox of checkboxes){
+        if (checkbox.checked) {
+            totalAmount += parseInt(checkbox.value);
+        }
+    }
+    priceBanner.textContent = `€{totalAmount}`;
+};
+
+form.addEventListener('change', priceCalc);
