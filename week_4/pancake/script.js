102d7d54f17d58b233574247aaf5ea6265fd0df6
Dana Popa <popa.dana100@gmail.com>
2024-02-13 14:27:05 +0200

    js update

--- week_4/pancake/script.js
+++ week_4/pancake/script.js
@@ -39,26 +39,77 @@
 
 
 const form = document.querySelector('.form-container');
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+const typeSelect = document.querySelector('#type');
+const button = document.querySelector('button');
 
-const priceCalc = () => {
-    // console.log('frwbfv');
+let total;
+let toppings = [];
+let extras = [];
 
-    const typeSelect = document.querySelector('#type');
-    const checkboxes = document.querySelectorAll("input[type='checkbox']");
-    const priceBanner = document.getElementById("totalPrice");
+const pancakePriceCalc = () => {
+  const totalPriceElement = document.querySelector('#totalPrice');
+  const priceBanner = document.querySelector('.price-banner');
 
-    // console.log(typeSelect.value);
-    // console.log(checkboxes);
+  total = parseInt(typeSelect.value);
+  totalPriceElement.textContent = `$${total}`;
 
-    let totalAmount = parseInt(typeSelect.value);
-    // console.log(totalAmount);
+  checkToppings();
 
-    for (const checkbox of checkboxes){
-        if (checkbox.checked) {
-            totalAmount += parseInt(checkbox.value);
-        }
+  priceBanner.animate(
+    [
+      { transform: 'scale(1)' },
+      { transform: 'scale(1.05)' },
+      { transform: ' scale(1)' },
+    ],
+    {
+      duration: 100,
+      iterations: 1,
     }
-    priceBanner.textContent = `€{totalAmount}`;
+  );
 };
 
-form.addEventListener('change', priceCalc);
+const addItem = (itemName, category) => {
+  if (category === 'toppings') {
+    toppings.push(itemName);
+  } else {
+    extras.push(itemName);
+  }
+};
+
+const checkToppings = () => {
+  toppings = [];
+  extras = [];
+
+  for (const item of checkboxes) {
+    const itemName = item.dataset.name;
+    const category = item.dataset.category;
+
+    if (item.checked) {
+      total += parseInt(item.value);
+      addItem(itemName, category);
+    } else {
+      removeItem(itemName, category);
+    }
+  }
+  console.log('toppings array', toppings);
+  console.log('extras array', extras);
+};
+
+const displayOrder = () => {
+  const customerName = document.querySelector('#customerName').value;
+  const orderType = document.querySelector('#order_type');
+  const orderToppings = document.querySelector('#order_toppings');
+  const orderExtras = document.querySelector('#order_extras');
+  const orderName = document.querySelector('#order_name');
+  const orderPrice = document.querySelector('#order_price');
+
+  orderType.textContent = typeSelect.selectedOptions[0].text;
+  orderToppings.textContent = toppings.join(', ');
+  orderExtras.textContent = extras.join(', ');
+  orderName.textContent = customerName;
+  orderPrice.textContent = total;
+};
+
+form.addEventListener('change', pancakePriceCalc);
+button.addEventListener('click', displayOrder);
