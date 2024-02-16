e16d1dd5a79f67d33620d51ca051cd9e42527541
Dana Popa <popa.dana100@gmail.com>
2024-02-16 14:35:47 +0200

    commit

--- week_6/objects/objects_1.js
+++ week_6/objects/objects_1.js
@@ -74,6 +74,7 @@ Values: Alex, Canada
 
 // Write your code here
 
+
 // Task 7: Using Object Methods
 /*
 Define a method inside the `person` object named `greet` that returns "Hello, my name is Alex".
