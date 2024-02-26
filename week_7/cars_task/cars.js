9ea1da1519f1cf6bdee57976986c2e762b3d3ef0
Dana Popa <popa.dana100@gmail.com>
2024-02-26 14:34:11 +0200

    first commit

---
+++ week_7/cars_task/cars.js
@@ -0,0 +1,12 @@
+'use strict';
+
+class car {
+    constructor(licensePlate, maker, model, currentOwner, price, color) {
+        this.licensePlate = licensePlate,
+        this.maker = maker,
+        this.model = model,
+        this.currentOwner = currentOwner,
+        this.price = price,
+        this.color = color
+    }
+}
