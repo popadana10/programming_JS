2432201de99e193db09b26f36b3d04d7199e629d
Dana Popa <popa.dana100@gmail.com>
2024-03-01 12:43:17 +0200

    solutions

--- week_7/cars_task/solutions/script.js
+++ week_7/cars_task/solutions/script.js
@@ -1,4 +1,8 @@
+'use strict';
+
 const addCarForm = document.querySelector('#addCar');
+
+const cars = [];
 
 class Car {
     constructor ( licence, maker, model, owner, price, color) {
@@ -11,9 +15,38 @@ class Car {
     }
 }
 
-const addCar = (event) => {
-    event.preventDefault();
-    console.log('addCar is triggered');
+const addCar = (e) => {
+    e.preventDefault();
+
+    const licence = document.querySelector('#licence').value;
+    const maker = document.querySelector('#maker').value;
+    const model = document.querySelector('#model').value;
+    const owner = document.querySelector('#owner').value;
+    const price = document.querySelector('#price').value;
+    const color = document.querySelector('#color').value;
+
+const newCar = new Car(licence, maker, model, price, color);
+cars.push(newCar);
+displayTable();
+
+addCarForm.reset();
+
 };
 
-addCarForm.addEventListener('submit', addCar);
+const displayTable =() => {
+    const table = document.querySelector('#carsTable');
+
+    table.innerHTML = table.rows[0].innerHTML;
+
+    cars.forEach(car => {
+        const row = table.insertRow(-1);
+
+        Object.values(car).forEach(text => {
+            const cell = row.insertCell(-1);
+            cell.textContent = text; 
+        });
+    });
+};
+
+addCarForm.addEventListener('submit', addCar)
+
