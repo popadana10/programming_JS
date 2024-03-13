dcf187f14b941063b1858a6c21d27ca341b2fac1
Dana Popa <popa.dana100@gmail.com>
2024-03-13 21:01:51 +0200

    portfolio final first commit

--- week_7/JS tricks/script.js
+++ week_7/JS tricks/script.js
@@ -1,7 +1,5 @@
-const backToTop = document.querySelector('#backToTop');
 const header = document.querySelector('header');
-const logo = document.querySelector('.logo');
-const mobile = document.querySelector('.responsive');
+const backToTop = document.querySelector('#backToTop');
 const mobButton = document.querySelector('.mobile');
 const nav = document.querySelector('nav ul');
 const menuItems = document.querySelectorAll('nav ul li a');
@@ -10,50 +8,40 @@ const overlay = document.querySelector('.overlay');
 const closeButton = document.querySelector('.closeButton');
 
 window.onscroll = function () {
-    scrollFunction();
-}
-
-const getToTop = () => {
-    document.body.scrollTop = 0; // tells the btn to go back to px 0  // for safari
-    document.documentElement.scrollTop = 0; // we have 2 scrollTop for 2 diff browsers // chrome, ff and others
-   // header.style.backgroundColor = 'pink';
-    // header.classList.remove('bg');
-    // header.classList.add('bg');
-       // header.classList.toggle('bg');
-}
-
-const mobileMenu = () => {
-    nav.classList.toggle('responsive');
-}
-
-const toggleModal = () => {
-    overlay.classList.toggle('visible');
-}
+  scrollFunction();
+};
 
 const scrollFunction = () => {
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-        backToTop.style.display = "block";
-        header.classList.add('bg');
-    } else {
-        backToTop.style.display = "none";
-        header.classList.remove('bg');
-    }
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backToTop.style.display = 'block';
+    header.classList.add('bg');
+  } else {
+    backToTop.style.display = 'none';
+    header.classList.remove('bg');
   }
+};
 
-backToTop.addEventListener('click', getToTop);
-mobButton.addEventListener('click', mobileMenu); 
-menuItems.forEach((item) => item.addEventListener('click', mobileMenu));
+const toggleModal = () => {
+  overlay.classList.toggle('visible');
+};
+
+const toggleMenu = () => {
+  nav.classList.toggle('responsive');
+};
+
+backToTop.addEventListener('click', () => {
+  document.body.scrollTop = 0; // for Safari
+  document.documentElement.scrollTop = 0; // Chrome, FF and others
+  // header.style.backgroundColor = 'orange';
+  header.classList.toggle('bg');
+});
+
+mobButton.addEventListener('click', toggleMenu);
+menuItems.forEach((item) => item.addEventListener('click', toggleMenu));
+
 modalButton.addEventListener('click', toggleModal);
 closeButton.addEventListener('click', toggleModal);
-/*
-backToTop.addEventListener('click', () => {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-});
-
-document.querySelector('#backToTop').addEventListener('click', () => {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-});
-
-*/
+overlay.addEventListener('click', toggleModal);
